fdd9ff0dfd8a0d3d26df54adefa1969ce766d903
EvgenPrushk <ogmprushak@mail.ru>
2021-02-07 23:19:09 +0300

    feat: fucntion getSymmetry

--- core.js
+++ core.js
@@ -1,48 +1,48 @@
-function clearCanvas (canvas) {
+function clearCanvas(canvas) {
     // более быстрый способ очистки canvas
     canvas.width = canvas.width;
 }
 
-function setCanvasSize (canvas, width, height) {
+function setCanvasSize(canvas, width, height) {
     canvas.width = width;
     canvas.height = height;
 }
 
-function drawCirlce (canvas, x, y, r, color) {
+function drawCirlce(canvas, x, y, r, color) {
     const context = canvas.getContext('2d');
     context.beginPath();
-    
-    context.fillStyle= color;
+
+    context.fillStyle = color;
     context.arc(x, y, r, 0, Math.PI * 2);
-    context.fill();       
+    context.fill();
 }
 
 function drawLine(canvas, x1, y1, x2, y2, width, color) {
     const context = canvas.getContext('2d');
     context.beginPath();
-    
+
     context.strokeStyle = color;
     context.lineWidth = width;
-    
+
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
-    context.stroke();       
+    context.stroke();
 }
 
 //добиваемся постаянного выполнения функции
-function tick (handler) {   
+function tick(handler) {
     timer(0);
     // пример рекрсивной функции
-    function timer (timestamp) {
+    function timer(timestamp) {
         handler(timestamp)
         // регистрирует вызов функции с частотой обновления экрана ( примерно 60 раз в секунду) и 1 раз вызывает ее
         requestAnimationFrame(timer);
-    // позволяет вызывать функцию с определенной периодичностью
-    // setInterval(handler, 0)
+        // позволяет вызывать функцию с определенной периодичностью
+        // setInterval(handler, 0)
     }
 }
 // расчет координат точки
-function getSymmetry (pointA, pointB) {
+function getCenterSymmetry(pointA, pointB) {
     const dx = pointB.x - pointA.x;
     const dy = pointB.y - pointA.y;
 
@@ -51,3 +51,24 @@ function getSymmetry (pointA, pointB) {
         y: pointA.y + 2 * dy,
     }
 }
+
+
+function getSymmetry(blackPoint, greenPoint, n) {
+    // квадрат разности по координате х + квадрат расзностей по координате y. Берет квадратный корень из этой суммы
+    const distance = ((blackPoint.x - greenPoint.x) ** 2 + (blackPoint.y - greenPoint.y) ** 2) ** 0.5;
+    // смотри определение угла через atan 2
+    const angle = Math.atan2 (blackPoint.y - greenPoint.y, blackPoint.x - greenPoint.x);
+    // определяем угол шага
+    const step = Math.PI * 2 / n;
+
+    const points = [];
+    for (let i = 0; i < n; i++) {
+        points.push({
+            x: greenPoint.x + distance * Math.cos(angle + step * i),
+            y: greenPoint.y + distance * Math.sin(angle + step * i),
+        })
+    }
+
+    return points;    
+}
+
--- script.js
+++ script.js
@@ -1,47 +1,55 @@
 const WIDTH = 500;
 const HEIGHT = 500;
 const R = 6;
+const N = 5;
 
 const CENTER_POINT = {
     x: WIDTH / 2,
     y: HEIGHT / 2,
-}
+};
 
 const appElement = document.querySelector('#app');
-const  basicCanvas = document.querySelector('#basic');
-const  viwerCanvas = document.querySelector('#viwer');
-const  colorElement = document.querySelector('#color');
+const basicCanvas = document.querySelector('#basic');
+const viwerCanvas = document.querySelector('#viwer');
+const colorElement = document.querySelector('#color');
 // не используем стрелочную функцию, чтобы не потерять контекст
 colorElement.addEventListener('change', function () {
     color = this.value;
-})
+});
 
-let mouse = {x: 0, y: 0};
-let pmouse = {x: 0, y: 0};
+let mouse = {
+    x: 0,
+    y: 0
+};
+let pmouse = {
+    x: 0,
+    y: 0
+};
 let color = 'black';
 
-
-
-
 setMouseWatcher(appElement, newMouse => {
-   // запоминаем предыдущее состояние мыши
+    // запоминаем предыдущее состояние мыши
     pmouse = mouse;
     // обновляем тукущее состояние мыши
     mouse = newMouse;
 
     // т.к. setMouseWatcher вызывается чаще, чем tick мы переносим отрисовку сюда
     if (mouse.left) {
-        const smouse = getSymmetry(mouse, CENTER_POINT);
+        const points = getSymmetry(mouse, CENTER_POINT, N);
 
-        if (!pmouse.left) {   
+        if (!pmouse.left) {
+            for (const point of points) {
+                drawCirlce(viwerCanvas, point.x, point.y, R, color);
+            }  
+            // drawCirlce(basicCanvas, mouse.x, mouse.y, R, color);
+            // drawCirlce(basicCanvas, smouse.x, smouse.y, R, color);
 
-            drawCirlce(basicCanvas, mouse.x, mouse.y, R, color);
-            drawCirlce(basicCanvas, smouse.x, smouse.y, R, color);
-             
-        }
-
-        else { 
-            const spmouse = getSymmetry(pmouse, CENTER_POINT)
+        } else {
+            const ppoints = getSymmetry(pmouse, CENTER_POINT, N)
+            for (let i = 0; i < N; i++) {
+                const element = array[i];
+                
+            }
             drawLine(basicCanvas, pmouse.x, pmouse.y, mouse.x, mouse.y, 2 * R, color);
             drawLine(basicCanvas, spmouse.x, spmouse.y, smouse.x, smouse.y, 2 * R, color);
         }
@@ -64,14 +72,12 @@ drawCirlce(basicCanvas, CENTER_POINT.x, CENTER_POINT.y, R, 'red');
 
 // })
 
-tick( timestamp => {
+tick(timestamp => {
     clearCanvas(viwerCanvas);
-    // рисуем новое положение мышки
-    drawCirlce(viwerCanvas, mouse.x, mouse.y, R, color);
-    // рисуем новое положение точки симетричной
-    const smouse = getSymmetry(mouse, CENTER_POINT);
-    drawCirlce(viwerCanvas, smouse.x, smouse.y, R, color);
+  
+    const points = getSymmetry(mouse, CENTER_POINT, 3);
 
-})
-
-
+    for (const point of points) {
+        drawCirlce(viwerCanvas, point.x, point.y, R, color);
+    }  
+});
